783c571ed225175bd8ca314b3017ed0493aedd70
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2020-03-03 11:05:04 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      ),
+    );
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('checking'),
+      ),
+    );
+  }
+}
